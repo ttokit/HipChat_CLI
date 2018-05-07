f9c76bd39e21168db34e5fd99a0c6347c4fddf3f
ibisyu <ibisyu7@gmail.com>
2018-05-07 21:22:35 +0900

    Add --help option

--- lib/hipchat_cli/cli.ex
+++ lib/hipchat_cli/cli.ex
@@ -1,17 +1,30 @@
 defmodule HipChatCLI.CLI do
   def main(argv) do
     argv
+    |> parse_argv
     |> proccess
   end
 
-  def proccess(argv) do
+  def parse_argv(argv) do
     parse = OptionParser.parse(argv,
-                               switches: [token: :string, room_id: :string],
-                               aliases:  [t: :token, r: :room_id])
+                               switches: [help: :boolean, token: :string, room_id: :string],
+                               aliases:  [h: :help, t: :token, r: :room_id])
 
     case parse do
+      {[help: true], _, _}
+        -> :help
       {[token: token_file, room_id: room_id], [message], _}
-        -> HipChatCLI.send_message(token_file, room_id, message)
+        -> [message, token: token_file, room_id: room_id]
     end
+  end
+
+  def proccess(:help) do
+    IO.puts """
+    usage: hipchat_cli (-h | --help) [-t | --token token_file_path] [-r | --room_id room_id] message
+    """
+  end
+
+  def proccess([message, token: token_file, room_id: room_id]) do
+    HipChatCLI.send_message(token_file, room_id, message)
   end
 end
